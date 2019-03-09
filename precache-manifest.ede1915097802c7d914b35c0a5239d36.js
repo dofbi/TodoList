e1836ad5fb2de8b3f45cc9626cb6cb7311a42990
Mamadou Diagne <genova@dofbi.com>
2019-03-09 02:42:08 +0000

    TodoList Deployement

---
+++ precache-manifest.ede1915097802c7d914b35c0a5239d36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "c47e1788eaeb840ea730",
+    "url": "/static/js/main.7e1bcc3d.chunk.js"
+  },
+  {
+    "revision": "fd6e8e9acdb20f71f911",
+    "url": "/static/js/2.a24e253b.chunk.js"
+  },
+  {
+    "revision": "c47e1788eaeb840ea730",
+    "url": "/static/css/main.62efc231.chunk.css"
+  },
+  {
+    "revision": "76854fdc5c164622dceff835ee9d8bd9",
+    "url": "/index.html"
+  }
+];
